c95ea6eb2941d859e17721e49ced1952612e1260
fullstacksk <fullstacksk@gmail.com>
2020-04-20 12:34:49 +0530

    Setup webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,55 @@
 const path = require('path');
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        filename: "bundle.js",
-        path: path.join(__dirname, 'public'),
-        publicPath: "/"
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                loader: ['style-loader', 'css-loader', 'sass-loader'],
-                test: /\.s?css$/
-            }
-        ]
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-}
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const MiniCssExtracter = new MiniCssExtractPlugin({
+        filename: "styles.css"
+    });
+    return {
+        entry: "./src/app.js",
+        output: {
+            filename: "bundle.js",
+            path: path.join(__dirname, 'public'),
+            publicPath: "/"
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                }, {
+                    test: /\.s?css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                publicPath: (resourcePath, context) => {
+                                    return path.relative(path.dirname(resourcePath), context) + '/';
+                                }
+                            }
+                        }, {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }, {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            MiniCssExtracter
+        ],
+        devtool: isProduction ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+};
